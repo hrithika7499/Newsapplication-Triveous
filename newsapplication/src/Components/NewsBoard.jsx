8ed25ceac896400144805ed42ae32d40ed668e9d
Hrithika <rhrithikha04@gmail.com>
2023-12-06 01:39:11 +0530

    cards done

--- newsapplication/src/Components/NewsBoard.jsx
+++ newsapplication/src/Components/NewsBoard.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react"
 import NewsItem from "./NewsItem";
 
-const NewsBoard = () => {
+const NewsBoard = ({category}) => {
 
     const[articles,setArticles]=useState([]);
     useEffect(()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
-        fetch.then(response=>response.json()).then(data=>setArticles(data.articles));
-    },[])
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url).
+        then(response=>response.json()).
+        then(data=>setArticles(data.articles));
+    },[category])
   return (
     <div>
       <h2 className="text-center">Latest<span className="badge bg-danger">News</span></h2>
--- newsapplication/src/Components/NewsItem.jsx
+++ newsapplication/src/Components/NewsItem.jsx
@@ -1,16 +1,20 @@
 
+import image from '../assets/newspic.jpeg';
 
-const NewsItem = () => {
+const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  )
-}
+    <div className="card bg-dark text-light mb-2 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "340px" }}>
+        
+      <img src={src? src:image} style={{ height: "200px", width: "100%", objectFit: "cover"  }}className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">{description ? description.slice(0, 90) : "Error in fetching data"}</p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default NewsItem
+export default NewsItem;
